8298f10d49dd1713cb866abbe1f5b07d87f378b7
Hélène <helene.loozen@gmail.com>
2016-03-14 14:29:57 +0100

    Camera optical flow with yaml
    Reading format change from "txt" to "yml"

--- CameraMotion/CameraMotion.cpp
+++ CameraMotion/CameraMotion.cpp
@@ -14,6 +14,37 @@ using namespace cv;
 using namespace std;
 
 const char ESC_KEY = 27;
+string dataline;
+
+void opticalflowArrows (Mat frame, vector<uchar> status, vector<Point2f>  keypoints1, vector<Point2f> keypoints2)
+{
+	// Source : Stavens_opencv_optical_flow
+	int scale = 7;
+	CvScalar color = CV_RGB(255,0,0);
+		for(int i = 0; i < status.size(); i++)
+		{
+			// Skip if no correspoendance found
+			if ( status.at(i) == 0 ) 
+				continue;
+			
+			// Points that will be used to draw the lines
+			Point2f p,q; 
+			p.x = (int) keypoints1.at(i).x;
+			p.y = (int) keypoints1.at(i).y;
+			q.x = (int) keypoints2.at(i).x;
+			q.y = (int) keypoints2.at(i).y;
+			
+			double angle = atan2( (double) p.y - q.y, (double) p.x - q.x );
+			double hypotenuse = sqrt( pow((p.y - q.y),2) + pow((p.x - q.x),2) );
+			
+			// Stretch the arrows to actually see them
+			q.x = (int) (p.x - scale * hypotenuse * cos(angle));
+			q.y = (int) (p.y - scale * hypotenuse * sin(angle));
+			
+			// Draw on frame1
+			arrowedLine( frame, q, p, color, 2, 8, 0, 0.35);
+		}
+}
 
 int main(int argc, char **argv) 
 {
@@ -26,17 +57,22 @@ int main(int argc, char **argv)
 		return -1;
     }
     
-    // Open the datafile
-    ifstream data_position("markers_position.txt");
-	ifstream data_corners("markers_corners.txt");
-	if(! data_position.is_open() || ! data_corners.is_open())
+    // Open the datafiles
+	FileStorage markersCenter("markers_centers.yml", FileStorage::READ);
+	FileStorage markersCorners("markers_corners.yml", FileStorage::READ);
+	if(! markersCenter.isOpened() || ! markersCorners.isOpened())
 	{
 		cerr <<"Error opening data files !" <<endl;
 		return -1;
 	}
 	
 	//Feature detector initialization
-	Ptr<FeatureDetector> detector = new FastFeatureDetector(10);
+	Ptr<FeatureDetector> detector = new FastFeatureDetector(50);
+	
+// 	Ptr<FeatureDetector> detector = new GoodFeaturesToTrackDetector(200);
+// 	detector->set("useHarrisDetector",true);
+// 	detector->set("minDistance",10.);
+	
 // 	Ptr<FeatureDetector> detector = new MserFeatureDetector();
 // 	detector->set("minDiversity",0.7);
 // 	detector->set("maxVariation",0.2);
@@ -58,49 +94,84 @@ int main(int argc, char **argv)
 	vector<float> err;
 	Mat frame1, frame2;
 	Mat frame_keypoints_out, frame_flow_out;
+	Point2f centers[10], corners[10][4];
 	
 	// Parameters
 	int bestPoints = 200;
-	int thickness = 2;
-	int scale = 7;
-	CvScalar color = CV_RGB(255,0,0);
 	
 	// Variables initialization for the screenshot option
 	string filename;
 	stringstream ss;
-	int o=0;
+	int o=0;	
 	
+	Mat centersMatrix, cornersMatrix;
+	markersCenter ["frame1"] >> centersMatrix;
+	markersCorners ["frame1"] >> cornersMatrix;
+	for(int i=0; i<10; ++i)
+		if(!(cornersMatrix.at<float>(0,i)<0 || cornersMatrix.at<float>(1,i)<0))
+		{
+			
+			vector < vector<Point> > contour;
+			contour.push_back(vector<Point>());
+			for(int j=0; j<4 ; j++)
+				{
+					contour[0].push_back(Point (cornersMatrix.at<float>((2*j),i),cornersMatrix.at<float>((2*j)+1,i)));
+				}
+			cout << cornersMatrix;	
+			cout << contour[0] << endl;
+			int thickness = max(abs(contour[0].at(0).x -contour[0].at(1).x),abs(contour[0].at(0).y - contour[0].at(2).y))+10;
+			drawContours(mask,contour,-1,Scalar::all(0),thickness);
+		}
 	
-	
-// 	vector<Point2f> corners[10];
-// 	//Point corners1[4], corners2[4], corners3[4], corners4[4], corners5[4], corners6[4] , corners7[4], corners8[4], corners9[4], corners10[4];
-// 	for(int i=0 ; i<4 ; ++i)
-// 	{
-// 		for(int j=0 ; j<10 ; ++j)
-// 			data_corners >> corners[j].push_back();
-// 		//data_corners >> corners1[i]>> corners2[i]>> corners3[i]>> corners4[i]>> corners5[i]>> corners6[i] >> corners7[i]>> corners8[i]>> corners9[i]>> corners10[i];
-// 	}
-// 	
-// 	cout<< corners[3] << endl;
-// 	
+			
 	// Do the first frame out of the main loop
 	capture >> frame1;
 	detector->detect(frame1, keypoints, mask);
 	KeyPointsFilter::retainBest(keypoints,bestPoints);
 	KeyPoint::convert(keypoints, kpt1);
 	
-	while(true)
+	stringstream frameNumber;
+	int frameCount = (int)  markersCenter["frameCount"];
+	
+	for(int frame = 2; frame <= frameCount; frame++ )
 	{
 		// Acquire new frame
 		capture >> frame2;
 		
 		// End when video finishes
-		if (frame2.empty())
+		if (frame2.empty() )
 			break;
 		
-		vector< vector<Point> > corners;
-		corners.push_back(vector<Point>());
+		// Mask creation
+		mask= Mat(capture.get(CV_CAP_PROP_FRAME_HEIGHT),capture.get(CV_CAP_PROP_FRAME_WIDTH), CV_8UC1,Scalar::all(225));
 		
+		// Retrive information about the center and the corners of the markers
+			
+		
+		for(int frame = 2; frame <= frameCount; frame++ )
+		{
+			frameNumber << "frame" << frame;
+			markersCenter [frameNumber.str()] >> centersMatrix;
+			markersCorners [frameNumber.str()] >> cornersMatrix;
+			frameNumber.str("");
+		}
+		
+		
+		// Create the mask to avoid taking feature in the markers
+		for(int i=0; i<10; ++i)
+			if(!(cornersMatrix.at<float>(0,i)<0 || cornersMatrix.at<float>(1,i)<0))
+			{
+				
+				vector < vector<Point> > contour;
+				contour.push_back(vector<Point>());
+				for(int j=0; j<4 ; j++)
+					{
+						contour[0].push_back((Point2f) (cornersMatrix.at<float>((2*j),i),cornersMatrix.at<float>((2*j)+1,i)));
+					}
+				
+				int thickness = max(abs(contour[0].at(0).x -contour[0].at(1).x),abs(contour[0].at(0).y - contour[0].at(2).y))+10;
+				drawContours(mask,contour,-1,Scalar::all(0),thickness);
+			}
 		
 		// Detecting keypoints
 		detector->detect(frame2, keypoints,mask);
@@ -108,43 +179,14 @@ int main(int argc, char **argv)
 		KeyPoint::convert(keypoints, kpt2);
 		kpt_tmp = kpt2;
 		
-// 		// Show video with keypoints
-// 		drawKeypoints(frame2, keypoints, frame_keypoints_out,Scalar::all(0));
-// 		namedWindow("Keypoints Image",0);
-// 		resizeWindow("Keypoints Image", 600,380);
-// 		imshow("Keypoints Image", frame_keypoints_out);
-		
 		
 		// Compute opticalflow
 		calcOpticalFlowPyrLK(frame1,frame2,kpt1,kpt2,status,err);
 		cout << (float) count(status.begin(),status.end(),1)/status.size() <<endl;
 		
 		// Showing arrow flow
-		// Source : Stavens_opencv_optical_flow
 		frame2.copyTo(frame_flow_out);
-		for(int i = 0; i < status.size(); i++)
-		{
-			// Skip if no correspoendance found
-			if ( status.at(i) == 0 ) 
-				continue;
-			
-			// Points that will be used to draw the lines
-			Point2f p,q; 
-			p.x = (int) kpt1.at(i).x;
-			p.y = (int) kpt1.at(i).y;
-			q.x = (int) kpt2.at(i).x;
-			q.y = (int) kpt2.at(i).y;
-			
-			double angle = atan2( (double) p.y - q.y, (double) p.x - q.x );
-			double hypotenuse = sqrt( pow((p.y - q.y),2) + pow((p.x - q.x),2) );
-			
-			// Stretch the arrows to actually see them
-			q.x = (int) (p.x - scale * hypotenuse * cos(angle));
-			q.y = (int) (p.y - scale * hypotenuse * sin(angle));
-			
-			// Draw on frame1
-			arrowedLine( frame_flow_out, q, p, color, thickness, 8, 0, 0.35);
-		}
+		opticalflowArrows (frame_flow_out, status, kpt1, kpt2);
 		
 		
 		// Show video with opticalflow
@@ -179,3 +221,4 @@ int main(int argc, char **argv)
     
     return 0;
 }
+
